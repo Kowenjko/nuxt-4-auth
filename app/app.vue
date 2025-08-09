7bdbb38fdf73fdc7314c1db0cc8b483546cccea2
Kowenjko <ortoswt@gmail.com>
2025-08-09 11:36:39 +0300

    create project

---
+++ app/app.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import test from '@/components/Test.vue'
+
+const testMy =()=>{
+  const a= 2;
+  const b= 'my test';
+
+
+}
+
+testMy()
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
